6ae23e5a6581f53eb86f2032f396282b44c7e96c
gsirbiladze <grigoli@gmail.com>
2019-10-25 22:28:48 -0700

    Update login_dialog.dart

--- login_dialog.dart
+++ login_dialog.dart
@@ -3,12 +3,15 @@ import 'package:flutter/material.dart';
 class LoginDialog extends StatelessWidget{
   LoginDialog({
     Key key,
-    this.borderColor = Colors.green,
-    this.borderWidth = 4,
+    this.color = Colors.blue,
+    this.borderWidth = 2,
     this.borderRoudness = 15,
+    this.alertDialogStile = true,
   }):super(key:key);
 
-  final Color borderColor;
+  final bool alertDialogStile;
+
+  final Color  color;
   final double borderWidth;
   final double borderRoudness;
 
@@ -18,20 +21,55 @@ class LoginDialog extends StatelessWidget{
     var uCtl = TextEditingController();
     var pCtl = TextEditingController();
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+    return alertDialogStile
+      ?AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderRoudness)),
+        content: Container(
+          child: _ActiveElements(color: color, borderWidth: borderWidth, borderRoudness: borderRoudness, uCtl: uCtl, pCtl: pCtl),
+        ),
+      )
+      :Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(borderRoudness),
+          child: Center(
+            child: _ActiveElements(color: color, borderWidth: borderWidth, borderRoudness: borderRoudness, uCtl: uCtl, pCtl: pCtl),
+          )
+        )
+      );
+  }
+}
+
+class _ActiveElements extends StatelessWidget {
+  const _ActiveElements({
+    Key key,
+    @required this.color,
+    @required this.borderWidth,
+    @required this.borderRoudness,
+    @required this.uCtl,
+    @required this.pCtl,
+  }) : super(key: key);
+
+  final Color color;
+  final double borderWidth;
+  final double borderRoudness;
+  final TextEditingController uCtl;
+  final TextEditingController pCtl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
             TextField(
               decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10),
                 enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: borderColor, width: borderWidth),
+                  borderSide: BorderSide(color: color, width: borderWidth),
                   borderRadius: BorderRadius.all(Radius.circular(borderRoudness)),
                 ),
                 labelText: 'Username',
                 border: OutlineInputBorder(
-                  borderSide: BorderSide(color: borderColor, width: borderWidth),
+                  borderSide: BorderSide(color: color, width: borderWidth),
                   borderRadius: BorderRadius.all(Radius.circular(borderRoudness)),
                 ),
               ),
@@ -42,35 +80,44 @@ class LoginDialog extends StatelessWidget{
               fallbackHeight: borderRoudness,
             ),
             TextField(
+              obscureText: true,
               decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10),
                 enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: borderColor, width: borderWidth),
+                  borderSide: BorderSide(color: color, width: borderWidth),
                   borderRadius: BorderRadius.all(Radius.circular(borderRoudness)),
                 ),
                 labelText: 'Password',
                 border: OutlineInputBorder(
-                  borderSide: BorderSide(color: borderColor, width: borderWidth),
+                  borderSide: BorderSide(color: color, width: borderWidth),
                   borderRadius: BorderRadius.all(Radius.circular(borderRoudness)),
                 ),
               ),
               controller: pCtl,
             ),
+            Placeholder(
+              color: Colors.transparent,
+              fallbackHeight: borderRoudness,
+            ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                RaisedButton(
+                FlatButton(
+                  color: color,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderRoudness)),
                   child: Text('Cancel'),
                   onPressed: ()=>Navigator.pop(context),
                 ),
-                RaisedButton(
+                FlatButton(
+                  color: color,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderRoudness)),
                   child: Text('Login'),
                   onPressed: ()=>Navigator.pop(context, {"username": uCtl.text, "password": pCtl.text}),
                 ),
               ],
             ),
-          ],
-        ),
-      ),    
+        ],
     );
   }
 }
